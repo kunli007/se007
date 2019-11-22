4f207d60aea8ecb958707d7399ae836d49b5b5d3
kunli007 <57168291+kunli007@users.noreply.github.com>
2019-11-22 22:29:50 +0700

    Add files via upload

---
+++ src/Bum.js
@@ -0,0 +1,57 @@
+import React,{Component} from 'react';
+import './Bum.css';
+
+class Bum extends Component {
+
+render(){
+    
+        return (
+          <div className="Appb" >
+      
+
+
+
+      
+      
+<div class="boxbum">
+
+       <ul >
+      <li ><a href="#home"><b>หมวดหมู่</b></a></li>
+      <li><a href="#news"><b>&nbsp;&nbsp;&nbsp;&nbsp;สุนัข&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-</b></a></li>
+      <li><a href="#contact"><b>&nbsp;&nbsp;&nbsp;&nbsp;แมว&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+</b></a></li>
+      <li><a href="#about"><b>&nbsp;&nbsp;&nbsp;&nbsp;อื่นๆ&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+</b></a></li>
+      <li><a href="#about"><b>&nbsp;&nbsp;&nbsp;&nbsp;พื้นที่&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+</b></a></li>
+</ul>
+  
+      <div class="center">
+        <div class="pagination">
+        <a href="#">Back</a>
+        <a href="#">1</a>
+        <a href="#">2</a>
+        <a href="#">3</a>
+        <a href="#">4</a>
+        <a href="#">...</a>
+        <a href="#">20</a>
+        <a href="#">Next</a>
+        </div>
+
+        <div class="center1">
+        <p class= "slid1">เรียงตาม</p>
+        <a href="#" className="slid"><i class='fas fa-sliders'></i></a> 
+
+        </div>
+        
+        </div>
+      </div> 
+
+      </div> 
+        ); 
+        
+      }
+    }
+      
+      export default Bum;
